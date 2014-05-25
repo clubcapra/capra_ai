f7ed471dd9108c83cd674ac657b46e602fb5f9c3
Gabriel K <g@brie.lk>
2014-05-25 11:38:29 -0400

    NavSatFix (GPS position) subscriber

--- src/ai_base.py
+++ src/ai_base.py
@@ -3,8 +3,10 @@
 import roslib; roslib.load_manifest('capra_ai')
 import rospy
 import actionlib
+from sensors.Gps import Gps
 from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
 from capra_msgs.msg import AiStatus, EStopStatus
+from sensor_msgs.msg import NavSatFix
 
 class AIBase(object):
     running = True
@@ -17,6 +19,7 @@ class AIBase(object):
         #self.client.wait_for_server()
         self._create_status_broadcaster()
         rospy.Subscriber("/capra_smartmotor/estop", EStopStatus, self._estop_subscriber)
+        rospy.Subscriber("/capra_gps", NavSatFix, self._gps_subscriber)
 
     def _create_status_broadcaster(self):
         self.status_publisher = rospy.Publisher('~status', AiStatus)
@@ -30,6 +33,9 @@ class AIBase(object):
     def _estop_subscriber(self, data):
         self.estop_listener(data.stopped)
 
+    def _gps_subscriber(self, data):
+        self.gps_listener(data.lat, data.lon)
+
     def send_goal(self, goal, wait=False):
         self.client.send_goal(goal)
         
@@ -41,4 +47,7 @@ class AIBase(object):
 
     def estop_listener(self, status):
         self.running = not status
+
+    def gps_listener(self, latitude, longitude):
+        self.Gps = Gps(latitude, longitude)
                 
---
+++ src/sensors/Gps.py
@@ -0,0 +1,5 @@
+class Gps:
+
+    def __init__(self, latitutde, longitude):
+        self.latitude = latitude
+        self. longtitude = longitude
