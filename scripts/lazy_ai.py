4854d6add456a2bb82237b3c74c8ef03790fbe21
Mathieu Gascon-Lefebvre <mathieuglefebvre@gmail.com>
2014-10-07 15:49:11 -0400

    Ajout d'un AI de base pour aider les nouveaux

--- scripts/lazy_ai.py
+++ scripts/lazy_ai.py
@@ -1,128 +1,88 @@
 #! /usr/bin/env python
 
-import roslib; roslib.load_manifest('capra_ai')
-import rospy
-from sensor_msgs.msg import LaserScan
+from base_ai import BaseAI
 import math
-from geometry_msgs.msg import *
 import numpy as np
-import cv2
-
-rospy.init_node('ai_demo_cones')
 
 
-#La taille de la carte generee (en m). Le robot est au milieu.
-map_size = rospy.get_param("~map_size", 6.0)
+class LazyAI(BaseAI):
 
-#La resolution de la carte generee
-map_resolution = rospy.get_param("~map_resolution", 0.01)
+    def __init__(self):
+        super(LazyAI, self).__init__("lazy_ai")
+        #Les angles ou le robot peut aller (A gauche seulement dans cet AI)
+        self.priority_angles = [int(v.strip()) for v in self.get_param("~priority_angles", "0, 10, 20, 30, 40, 50, 60").split(",")]
+        #La taille de la carte generee (en m). Le robot est au milieu.
+        self.map_size = self.get_param("~map_size", 6.0)
+        #La resolution de la carte generee
+        self.map_resolution = self.get_param("~map_resolution", 0.01)
+        #Le rayon du cercle de verification (pour savoir si le robot passe)
+        self.robot_radius = self.get_param("~robot_radius", 0.7)
+        #La distance par rapport au range finder ou le cercle est deplace
+        self.displacement_check = self.get_param("~displacement_check", 0.6)
+        #La vitesse du robot
+        self.speed = self.get_param("~speed", 0.5)
 
-#Le rayon du cercle de verification (pour savoir si le robot passe)
-robot_radius = rospy.get_param("~robot_radius", 0.7)
+        self.size = int(math.ceil(self.map_size / self.map_resolution))
+        self.max_dist = self.map_size / 2.0
+        self.robot_area = self.create_circle(int(self.robot_radius/self.map_resolution), (int(self.size/2.0), int(self.size/2.0)))
 
-#La distance par rapport au range finder ou le cercle est deplace
-displacement_check = rospy.get_param("~displacement_check", 0.6)
+    @staticmethod
+    def find_dist(dist, angle):
+        x = dist * math.cos(angle)
+        y = dist * math.sin(angle)
 
-#La vitesse du robot
-speed = rospy.get_param("~speed", 0.5)
+        return x, y
 
-#Les angles ou le robot peut aller
-priority_angles = [int(v.strip()) for v in rospy.get_param("~priority_angles", "0, 10, 20, 30, 40, 50, 60").split(",")]
+    @staticmethod
+    def create_circle(radius, center):
+        circle = []
+        for x in range(-radius, 1):
+            for y in range(-radius, 1):
+                if x * x + y * y < radius * radius:
+                    circle.append((x + center[0], y + center[1]))
+                    circle.append((-x + center[0], y + center[1]))
+                    circle.append((x + center[0], -y + center[1]))
+                    circle.append((-x + center[0], -y + center[1]))
+        return circle
 
-scan = LaserScan()
-scan_received = False
+    @staticmethod
+    def calculate_sum(mat, circle, center):
+        s = 0
+        for p in circle:
+            x = p[0] + center[0]
+            y = p[1] + center[1]
+            s += mat[x][y]
+        return s
 
-def find_dist(dist, angle):
-    x = dist * math.cos(angle)
-    y = dist * math.sin(angle)
-
-    return x, y
-
-
-def rf_callback(msg):
-    global scan, scan_received
-    scan = msg
-    scan_received = True
-
-
-def create_circle(radius, center):
-    circle = []
-    for x in range(-radius, 1):
-        for y in range(-radius, 1):
-            if x * x + y * y < radius * radius:
-                circle.append((x + center[0], y + center[1]))
-                circle.append((-x + center[0], y + center[1]))
-                circle.append((x + center[0], -y + center[1]))
-                circle.append((-x + center[0], -y + center[1]))
-    return circle
-
-def calculate_sum(mat, circle, center):
-    sum = 0
-    for p in circle:
-        x = p[0] + center[0]
-        y = p[1] + center[1]
-        sum += mat[x][y]
-    return sum
-
-def show_matrix(mat, name):
-    m = np.flipud(mat)
-    m = np.fliplr(m)
-    im = np.asarray(m)
-    cv2.imshow(name, im)
-    cv2.waitKey(10)
-
-cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=4)
-rospy.Subscriber('/scan', LaserScan, rf_callback)
-rate = rospy.Rate(10)
-
-size = int(math.ceil(map_size / map_resolution))
-max_dist = map_size / 2.0
-robot_area = create_circle(int(robot_radius/map_resolution), (int(size/2.0), int(size/2.0)))
-cost = 0
-
-while not rospy.is_shutdown():
-    if scan_received:
-        m = np.zeros((size, size))
-
+    def loop(self):
+        m = np.zeros((self.size, self.size))
+        cost = 0
+        scan = self.get_scan()
         angle = scan.angle_min
+
         for p in scan.ranges:
             if p < 3.0:
-                if not ( p < 1.0 and abs(angle) > math.pi / 2):
-                    if p < 1 and p > 0.01:
-                        pass
-                    px, py = find_dist(p, angle)
-                    x = int((px + max_dist) / map_resolution)
-                    y = int((py + max_dist) / map_resolution)
+                if not (p < 1.0 and abs(angle) > math.pi / 2):
+                    px, py = self.find_dist(p, angle)
+                    x = int((px + self.max_dist) / self.map_resolution)
+                    y = int((py + self.max_dist) / self.map_resolution)
                     m[x][y] = 1
 
             angle += scan.angle_increment
 
-        for angle in priority_angles:
+        for angle in self.priority_angles:
             angle = math.radians(angle)
-            disp_x = int((displacement_check * math.cos(angle))/map_resolution)
-            disp_y = int((displacement_check * math.sin(angle))/map_resolution)
-            cost = calculate_sum(m, robot_area, (disp_x, disp_y))
+            disp_x = int((self.displacement_check * math.cos(angle))/self.map_resolution)
+            disp_y = int((self.displacement_check * math.sin(angle))/self.map_resolution)
+            cost = self.calculate_sum(m, self.robot_area, (disp_x, disp_y))
             if cost <= 1.0:
                 break
-#####
-        print cost
-        m2 = m.copy()
-        for p in robot_area:
-            m2[p[0] + disp_x][p[1]+disp_y] = 1
-        
-        m2[size/2][size/2]=0; m2[size/2 + 1][size/2]=0; m2[size/2 - 1][size/2]=0; m2[size/2][size/2 + 1]=0; m2[size/2][size/2 - 1]=0;
-        show_matrix(m2, "aaa")
-#####
 
-        print speed
-        vel = Twist()
         if cost <= 1:
-             vel.linear.x = speed * math.cos(angle)
-             vel.angular.z = speed * math.sin(angle)
+            self.set_velocity(self.speed * math.cos(angle), self.speed * math.sin(angle))
         else:
-            vel.linear.x = 0
-            vel.angular.z = speed
+            self.set_velocity(0, self.speed)
 
-        cmd_vel.publish(vel)
 
-    rate.sleep()    
+ai = LazyAI()
+ai.run()
--- scripts/sensors/Gps.py
+++
@@ -1,5 +0,0 @@
-class Gps:
-
-    def __init__(self, x, y):
-        self.x = x
-        self. y = y
